2a9ac4093587353ede1a0e5720e71241e6a9f551
Shawn Ellis <seellis1@student.fullsail.edu>
2025-08-18 17:48:35 -0400

    About page/ About.jsx updated and completed, brining api to UI and completion of the Home page in progress

--- client/src/components/WorkoutForm.jsx
+++ client/src/components/WorkoutForm.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 
 export default function WorkoutForm(){
     return(
         <form action="">
-
+            
         </form>
     )
 }
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -11,6 +11,11 @@ export default function AboutPage() {
       transition={{ duration: 0.8, delay: 0.3, ease: [0, 0.71, 0.2, 1.01] }}
     >
       <hr style={styles.hrStyling} />
+      <h1>About</h1>
+
+      <div style={styles.contentStyling}>
+        <p><span style={styles.logoStyling}>FitnessTrackerX</span> is an app that helps you track your workouts seamlessly. Offering a free plan for basic usage and a <strong>low budget plan</strong> which offers <b>unlimited fitness progress tracking, unlimited workout tracking, and access to our weekly workout plans</b>, that cater to your personal needs! </p>
+      </div>
     </motion.section>
   );
 }
@@ -21,4 +26,17 @@ const styles = {
     height: "0.1rem",
     backgroundColor: "grey",
   },
+  logoStyling: {
+    fontWeight: 'bolder',
+    color: "#001BB7",
+    textDecoration: "underline",
+  },
+  contentStyling: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "0 auto",
+    width: "75%",
+
+  }
 };
